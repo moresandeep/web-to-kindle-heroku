85c3fa666b7438d9c53dd07df61ac090e4476a67
Sandeep Moré <moresandeep@gmail.com>
2023-05-01 07:14:45 -0400

    use nodejs to take screenshots

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const path = require('path');
 const puppeteer = require('puppeteer');
 const execFile = require('child_process').execFile;
 const fs = require('fs');
+const sharp = require('sharp');
 
 const PORT = process.env.PORT || 3000;
+
+const FILENAME = 'screenshot.png';
+const PATH = '/tmp/';
+const CONVERTED_FILE = 'screenshot_grey.png'
+
+//https://merrysky.net/forecast/Hudson,%20OH/us
+const URL = 'https://world-weather.info/forecast/usa/hudson_6/';
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -14,15 +22,15 @@ express()
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.setViewport({ width: 600, height: 800 });
-    await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
+    await page.goto(process.env.SCREENSHOT_URL || URL);
     await page.screenshot({
-      path: '/tmp/screenshot.png',
+      path: PATH+FILENAME,
     });
 
     await browser.close();
 
-    await convert('/tmp/screenshot.png');
-    screenshot = fs.readFileSync('/tmp/screenshot.png');
+    await convert(PATH, FILENAME);
+    screenshot = fs.readFileSync(PATH+CONVERTED_FILE);
 
     res.writeHead(200, {
       'Content-Type': 'image/png',
@@ -32,8 +40,27 @@ express()
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+/* use sharp library insted of imagemagic convert utility */
+function convert(PATH, FILENAME) {
+  return new Promise((resolve, reject) => {
+    sharp(PATH+FILENAME)
+    .greyscale() // make it greyscale
+    .resize({
+      width: 600,
+      height: 800,
+      fit: sharp.fit.fill,
+      position: sharp.gravity.center
+    })
+    .linear(1.5, 0) // increase the contrast
+    .toFile(PATH+CONVERTED_FILE)
+    .then( data => { resolve();})
+    .catch( err => { console.error({err});
+    reject();});
+  });
 
-function convert(filename) {
+}
+
+function convert1(filename) {
   return new Promise((resolve, reject) => {
     const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
     execFile('convert', args, (error, stdout, stderr) => {
